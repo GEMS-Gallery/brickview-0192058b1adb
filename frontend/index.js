a756e68ebac6df72e99ac739ea500dc5f66e56bd
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-18 10:39:58 -0400

    01920593-1ed4-7333-8c59-583563db4fa7

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const imageContainer = document.getElementById('image-container');
     const img = document.createElement('img');
-    img.src = 'https://images.thdstatic.com/productImages/85ee3ead-5053-4f94-a991-044fe1a396e6/svn/leaf-green-trafficmaster-needlepunch-carpet-7pd5n620144h-64_1000.jpg';
-    img.alt = 'Green Carpet';
-    img.className = 'carpet-image';
+    img.src = 'https://www.coremarkmetals.com/files/image/large/STEEL_BEAM_JUNIOR_OMS.jpg';
+    img.alt = 'Steel Beam';
+    img.className = 'steel-beam-image';
     imageContainer.appendChild(img);
 });

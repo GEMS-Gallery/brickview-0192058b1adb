127f98a310b8b01204b7974931c213d816ede390
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-18 10:41:55 -0400

    01920594-f648-7333-8c59-dee9967d5a74

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const imageContainer = document.getElementById('image-container');
     const img = document.createElement('img');
-    img.src = 'https://www.coremarkmetals.com/files/image/large/STEEL_BEAM_JUNIOR_OMS.jpg';
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/I-Beam_002.JPG/440px-I-Beam_002.JPG';
     img.alt = 'Steel Beam';
     img.className = 'steel-beam-image';
     imageContainer.appendChild(img);

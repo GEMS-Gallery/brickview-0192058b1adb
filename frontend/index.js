ae8a3445768c476559db65be8c8d8385e321942b
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-18 10:35:49 -0400

    0192058f-5438-7333-8c58-f01b1dadee90

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const imageContainer = document.getElementById('image-container');
     const img = document.createElement('img');
-    img.src = 'https://t3.ftcdn.net/jpg/01/36/83/78/360_F_136837876_CHgC2LxHviEPGOX2ifuOFBnhDvHX5HV8.jpg';
-    img.alt = 'Brick Wall';
+    img.src = 'https://m.media-amazon.com/images/I/61x4+ia7+NL.jpg';
+    img.alt = 'Brick';
     img.className = 'brick-image';
     imageContainer.appendChild(img);
 });

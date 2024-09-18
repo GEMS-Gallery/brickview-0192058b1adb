26ce90dad4163f94f62e67d047ebb77853858fc1
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-18 10:33:38 -0400

    0192058d-4db9-7333-8c58-3d72fd75107f

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const imageContainer = document.getElementById('image-container');
     const img = document.createElement('img');
-    img.src = 'https://mobileimages.lowes.com/productimages/4d55aa9a-b645-48ec-9a9e-7e6480283eea/02592174.jpg?size=pdhism';
+    img.src = 'https://images.thdstatic.com/productImages/d9c8f1b5-9764-4b5e-a9d3-7303f0383fa3/svn/bricks-red0126mco-64_1000.jpg';
     img.alt = 'Brick';
     img.className = 'brick-image';
     imageContainer.appendChild(img);
